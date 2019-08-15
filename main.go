87eac93f6590c80aaae21caeef0fb044633affac
Nathaniel Read <nathaniel@northfolk.co.uk>
2019-08-15 16:37:35 +0100

    :sparkles: Basic email retrival

--- main.go
+++ main.go
@@ -7,8 +7,11 @@ import (
     "github.com/jprobinson/eazye"
     "os"
     "strconv"
+    "runtime"
+    //"sync"
 )
 
+var procs = runtime.NumCPU()
 
 func main() {
     fmt.Println("hello world")
@@ -25,8 +28,32 @@ func main() {
     rounded := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
 
     fmt.Println(rounded)
-    
-    mail, err := eazye.GetUnread(mailbox, false, false)
-    fmt.Println("done woo")
-    fmt.Println(mail,err)
+
+    mail, err := eazye.GetUnread(mailbox, true, false)
+    emailCount := len(mail)
+
+    // Just double check 
+    fmt.Println(emailCount, err)
+
+    parseMessages(mail[0])
+
 }
+
+func parseMessages(mail eazye.Email){
+    fmt.Println("Here I am", string(mail.HTML))
+}
+
+// Concurrent stuff to return to 
+//
+//     // concurrency time 
+//     var parsers sync.WaitGroup
+// 	for i := 0; i < emailCount; i++ {
+// 		parsers.Add(1)
+// 		// multi goroutines so we can utilize the CPU while waiting for URLs
+// 		go parseMessages(mail[i])
+//     }
+
+    
+//     fmt.Println("done woo")
+//     //fmt.Println(mail,err)
+// }
